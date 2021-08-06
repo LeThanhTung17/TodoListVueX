7d3b7e478511cee54e220c5fab1c6cb04abfd55a
Le Thanh Tung <42868337+LeThanhTung17@users.noreply.github.com>
2021-08-06 17:27:21 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Button type="primary" :loading="false" title="Detail" />
+    <Button type="success" :loading="false" title="Success" />
+    <Button type="danger" :loading="true" title="Delete" />
+  </div>
+</template>
+
+<script>
+import Button from './components/Button/index.vue';
+export default {
+  name: 'App',
+  components: { Button }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Loading/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <div class="loading"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading'
+};
+</script>
+
+<style>
+@import './styles.css';
+</style>
